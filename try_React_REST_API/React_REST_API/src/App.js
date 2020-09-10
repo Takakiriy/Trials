59e95fe32cc1b66ce047b685d18382e797be650a
Takakiri <takakiriy@gmail.com>
2020-09-10 12:24:23 +0900

    Added  try_React_REST_API

---
+++ try_React_REST_API/React_REST_API/src/App.js
@@ -0,0 +1,58 @@
+﻿import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axiosBase from 'axios';
+
+class  App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            <input type="button" onClick={this.handleButton.bind(this)} value="HTTP GET"/><br/>
+            {this.state.html}
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+  constructor() {
+     super();
+     this.state = {html: ""};
+     this.axios = axiosBase.create({
+       baseURL: '__BaseURL__',
+       headers: {
+         'Content-Type': 'application/json',
+         'X-Requested-With': 'XMLHttpRequest'
+       },
+       responseType: 'json'  
+     });
+   }
+
+  handleButton() {
+    this.axios.get('/__RelativeURL__')
+    .then( (response) => {
+      console.log(response);
+      this.setState({html: response.data});
+    })
+    .catch( (err) => {
+      console.log(err);
+      this.setState({html: "Error"});
+    })
+  }
+}
+
+export default App;
---
+++ try_React_REST_API/React_REST_API_base/run_clean.bat
@@ -0,0 +1,9 @@
+@echo off
+PATH=C:\Program Files\Git\usr\bin;%PATH%
+
+set commands=
+set commands=%commands%  cd  $(cygpath --unix '%~d0%~p0');
+set commands=%commands%  echo  'rm -rf node_modules';
+set commands=%commands%  rm -rf node_modules;
+
+start "no-title" "C:\Program Files\Git\git-bash.exe"  -c "%commands%"

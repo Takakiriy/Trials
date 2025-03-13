9342d80fd3e7090f9d4bd24a080aa5af4cc7be34
Your Name <you@example.com>
2025-03-13 22:22:17 +0900

    Delete duplicated priject.

--- try_TypeScript_Node_js_unnavigatable/__app__.command
+++
@@ -1,7 +0,0 @@
-
-# chmod +x __app__.command
-this_script_path="$0"
-this_folder="${this_script_path%/*}"
-export  NODE_PATH=${this_folder}/node_modules
-
-node ${this_folder}/build/app.js $*
--- try_TypeScript_Node_js_unnavigatable/build/app.d.ts
+++
@@ -1,8 +0,0 @@
-declare function getSecondaryHost(): string;
-declare function getConfig(): Config;
-declare function getServerConfig(): Config;
-declare function getSiteConfig(): Config;
-declare function buildConfig(site: Config): Config;
-interface Config {
-    dummy: string;
-}
